713dcd997e7adb60b0486ed0e92412f3d20acf90
陆光中 <luke_guangzhong@petalmail.com>
2025-06-30 23:10:53 +0800

    add uint32 test case

--- test/src/test_cvt_to_uint32.c
+++ test/src/test_cvt_to_uint32.c
@@ -66,8 +66,22 @@ CU_TestInfo test_uint32_conversion[] = {
     CU_TEST_INFO_NULL,
 };
 
+CU_TestInfo test_uint32_overflow[] = {
+    {"Reject conversion of JSON number to Protobuf uint32 on overflow",   test_cvt_json_number_overflow_uint32 },
+    {"Reject conversion of JSON number to Protobuf uint32 on underflow",  test_cvt_json_number_underflow_uint32},
+    {"Reject conversion of JSON string to Protobuf uint32 on overflow",   test_cvt_json_str_overflow_uint32    },
+    {"Reject conversion of JSON string to Protobuf uint32 on underflow",  test_cvt_json_str_underflow_uint32   },
+    {"Accept conversion of JSON number to Protobuf uint32 at UINT32_MAX", test_cvt_json_number_max_uint32      },
+    {"Accept conversion of JSON number to Protobuf uint32 at UINT32_MIN", test_cvt_json_number_min_uint32      },
+    {"Accept conversion of JSON string to Protobuf uint32 at UINT32_MAX", test_cvt_json_str_max_uint32         },
+    {"Accept conversion of JSON string to Protobuf uint32 at UINT32_MIN", test_cvt_json_str_min_uint32         },
+    CU_TEST_INFO_NULL,
+};
+
 CU_SuiteInfo suites[] = {
-    {"Convert JSON to Protobuf int64 field", init_sutie_name, cleanup_sutie_name, setup_successful_conversion, teardown_successful_conversion, test_uint32_conversion},
+    {"Convert JSON to Protobuf int64 field",                  init_sutie_name, cleanup_sutie_name, setup_successful_conversion, teardown_successful_conversion, test_uint32_conversion},
+    {"Convert JSON to Protobuf int32 with overflow handling", init_sutie_name, cleanup_sutie_name, setup_successful_conversion, teardown_successful_conversion,
+     test_uint32_overflow                                                                                                                                                             },
     CU_SUITE_INFO_NULL,
 };
 
@@ -149,6 +163,102 @@ test_cvt_json_number_to_oneof_uint32(void)
     CU_ASSERT_EQUAL(e, J2P_EXPT_SUCCESS);
     CU_ASSERT_EQUAL(msg->oneof_uint32, oneof_uint32_value);
     CU_ASSERT_EQUAL(msg->test_oneof_case, field_desc->id);
+}
+
+void
+test_cvt_json_number_overflow_uint32(void)
+{
+    uint64_t value = (uint64_t)UINT32_MAX + 1;
+
+    cJSON_AddNumberToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_VALUE_OVERFLOW);
+    CU_ASSERT_EQUAL(msg->f_uint32, 0);
+}
+
+void
+test_cvt_json_number_underflow_uint32(void)
+{
+    int64_t value = (int64_t)-1;
+
+    cJSON_AddNumberToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_VALUE_OVERFLOW);
+    CU_ASSERT_EQUAL(msg->f_uint32, 0);
+}
+
+void
+test_cvt_json_str_overflow_uint32(void)
+{
+    const char* value = "4294967296";
+
+    cJSON_AddStringToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_VALUE_OVERFLOW);
+    CU_ASSERT_EQUAL(msg->f_uint32, 0);
+}
+
+void
+test_cvt_json_str_underflow_uint32(void)
+{
+    const char* value = "-1";
+
+    cJSON_AddStringToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_VALUE_OVERFLOW);
+    CU_ASSERT_EQUAL(msg->f_uint32, 0);
+}
+
+void
+test_cvt_json_number_max_uint32(void)
+{
+    uint32_t value = UINT32_MAX;
+
+    cJSON_AddNumberToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_SUCCESS);
+    CU_ASSERT_EQUAL(msg->f_uint32, value);
+}
+
+void
+test_cvt_json_number_min_uint32(void)
+{
+    uint32_t value = 0;
+
+    cJSON_AddNumberToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_SUCCESS);
+    CU_ASSERT_EQUAL(msg->f_uint32, value);
+}
+
+void
+test_cvt_json_str_max_uint32(void)
+{
+    const char* value = "4294967295";
+
+    cJSON_AddStringToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_SUCCESS);
+    CU_ASSERT_EQUAL(msg->f_uint32, UINT32_MAX);
+}
+
+void
+test_cvt_json_str_min_uint32(void)
+{
+    const char* value = "0";
+
+    cJSON_AddStringToObject(root, uint32_field_name, value);
+
+    j2p_expt_t e = cvt_json_2_pb_number(root, cJSON_GetObjectItem(root, uint32_field_name), (ProtobufCMessage*)msg, uint32_field_name);
+    CU_ASSERT_EQUAL(e, J2P_EXPT_SUCCESS);
+    CU_ASSERT_EQUAL(msg->f_uint32, 0);
 }
 
 /******************************************************************************/
