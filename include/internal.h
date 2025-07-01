4ff689d05c9847240fdeab85f1827f4d9f9c9149
陆光中 <luguangzhong@hitrontech.com>
2025-07-01 12:33:53 +0800

    remove deprecated code

--- include/internal.h
+++ include/internal.h
@@ -24,14 +24,4 @@ j2p_expt_t cvt_single_uint32_t(const cJSON* const item, uint32_t* const field);
 
 j2p_expt_t cvt_single_uint64_t(const cJSON* const item, uint64_t* const field);
 
-// j2p_expt* cvt_single_uint64_t(const cJSON* const root, const cJSON* const item, uint64_t* const field);
-
-// j2p_expt* cvt_single_float(const cJSON* const root, const cJSON* const item, float* const field);
-
-// j2p_expt* cvt_single_double(const cJSON* const root, const cJSON* const item, double* const field);
-
-// j2p_expt* cvt_single_string(const cJSON* const root, const cJSON* const item, char** const field);
-
-// j2p_expt* cvt_single_bool(const cJSON* const root, const cJSON* const item, bool* const filed, const string_bool_convertor bool_cvt);
-
 bool default_string_bool_convertor(const char* const str);
